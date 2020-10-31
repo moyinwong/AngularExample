56d1ca2eb308078260fb142387013ba9d62ef3fb
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-10-31 21:59:19 +0800

    add repo to github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-dream-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-app');
+    expect(app.name).toEqual('my-dream-app');
   });
 
   it('should render title', () => {
